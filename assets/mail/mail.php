cb26fb80253896e21413d097d394200376a484d8
Well-appedidos <wellington.lira@appedidos.com.br>
2022-03-28 11:30:29 -0300

    change repository

---
+++ assets/mail/mail.php
@@ -0,0 +1,22 @@
+<?php
+$name = $_POST['name'];
+$subject = $_POST['subject'];
+$email = $_POST['email'];
+$phone = $_POST['phone'];
+$message = $_POST['message'];
+
+$email_message = "
+
+Name: ".$name."
+Subject: ".$subject."
+Email: ".$email."
+Phone: ".$phone."
+Message: ".$message."
+
+";
+
+mail ("contato@wellsistemasweb.com" , "New Message", $email_message);
+header("location: ../../mail-success.html");
+?>
+
+
